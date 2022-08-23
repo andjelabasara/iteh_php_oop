3743563f0166ee1cc1c8e9b226e3211a427ed2ae
andjelabasara <andjela.basara@gmail.com>
2022-08-23 19:03:45 +0200

    dodato sortiranje i pretraga

--- home.php
+++ home.php
@@ -20,7 +20,42 @@
     <title>Document</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+  
+    
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 <style>
+    body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+.input-container {
+  display: -ms-flexbox; 
+  display: flex;
+  width: 100%;
+  margin-bottom: 15px;
+}
+
+.icon {
+  padding: 10px;
+  background: dodgerblue;
+  color: white;
+  min-width: 50px;
+  text-align: center;
+}
+
+.input-field {
+ 
+  padding: 10px;
+  outline: none;
+    width: 100%;
+}
     .mainPart{
         padding: 150px;
     }
@@ -29,9 +64,17 @@
 <body>
     
     <div class="mainPart">
+    <input type="text" id="myInput" onkeyup="search()" placeholder="Search for names.." title="Type in a name" style="float:right;font-size:25px;border:none;
+    border-radius: 5px;">   <i class="fa fa-search" aria-hidden="true" style="float:right; font-size:30px;padding:4px"></i>
+
+
     <button type="button" class="btn btn-success"  data-toggle="modal" data-target="#addModal">Dodaj</button>
-    <br>
-        <table class="table">
+    <button type="button" class="btn btn-secondary"   onclick="sortTableAsc()"  ><i class="fa fa-sort" aria-hidden="true"></i>  Sort table by price ascending</button>  
+    <button type="button" class="btn btn-secondary"   onclick="sortTableDesc()"  ><i class="fa fa-sort" aria-hidden="true"></i>  Sort table by price descending</button>                    
+
+
+    <br><br><br>
+        <table class="table" id="tabela">
         <thead>
             <tr>
             <th scope="col">ID</th>
--- js/main.js
+++ js/main.js
@@ -99,3 +99,83 @@ $('#addform').submit(function () {
         console.error('Greska: ' + textStatus, errorThrown);
     });
 });
+
+
+function search(){
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tabela");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";
+        }
+        }       
+    }
+}
+
+
+function sortTableAsc(){
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("tabela");
+  switching = true; 
+  while (switching) { 
+    switching = false;
+    rows = table.rows;
+    console.log(rows); 
+    for (i = 1; i < (rows.length - 1); i++) { 
+      shouldSwitch = false; 
+      x = rows[i].getElementsByTagName("td")[3];
+      y = rows[i + 1].getElementsByTagName("td")[3]; 
+      console.log(x);
+      if (  x.innerText> y.innerText) { 
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) { 
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+    
+}
+ 
+}
+
+
+
+
+function sortTableDesc(){
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("tabela");
+    switching = true; 
+    while (switching) { 
+      switching = false;
+      rows = table.rows;
+      console.log(rows); 
+      for (i = 1; i < (rows.length - 1); i++) { 
+        shouldSwitch = false; 
+        x = rows[i].getElementsByTagName("td")[3];
+        y = rows[i + 1].getElementsByTagName("td")[3]; 
+        console.log(x);
+        if (  x.innerText< y.innerText) { 
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) { 
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+      
+  }
+   
+  }
+
+ 
